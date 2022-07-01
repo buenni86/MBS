58c7037cf209a5b8bf568d7a5a6f8b77fd763ba6
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-07-01 16:27:00 +0200

    Initial

---
+++ skript.js
@@ -0,0 +1,98 @@
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+
+bootstrapExtra().then(() => {
+    console.log('Scripting API Extra ready');
+}).catch(e => console.error(e));
+
+WA.onInit(async () => {
+    var currentPopup = undefined;
+    var isCoWebSiteOpened =  false;
+
+    function closePopUp(){
+        if (currentPopup !== undefined) {
+            currentPopup.close();
+            currentPopup = undefined;
+        }
+    }
+
+    var zoneNeedHelp = "needHelp";
+    var zoneFeedback = "feedback";
+    var zoneDBPlanet = "DBPlanet";
+    var urlTutorial = "https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure-erste-schritte";
+    var urlFeedback = "https://forms.office.com/Pages/ResponsePage.aspx?id=nC2noeZJbU-a9lqvoRg7_SoZREurD2tJrJwSfyf3bX5UNEczWlNIRkJDNjNUUjZKODUyVE9aNjQ0Uy4u";
+    var urlDBPlanet = "https://db-planet.deutschebahn.com/pages/telefonie/apps/content/workadventure";
+
+    WA.room.onEnterZone(zoneNeedHelp, () => {
+    currentPopup =  WA.ui.openPopup("needHelpPopup","Tutorial\nansehen?",[
+            {
+                label: "OK",
+                className:"primary",
+                callback: (popup => {
+                    WA.nav.openTab(urlTutorial);
+                    isCoWebSiteOpened = true;
+                    closePopUp();
+                })
+            }]);
+    })
+
+    WA.room.onLeaveZone(zoneNeedHelp, () =>{
+        closePopUp();
+
+        if (isCoWebSiteOpened) {
+            WA.nav.closeCoWebSite();
+            isCoWebSiteOpened = false;
+        }
+    })
+
+    WA.room.onEnterZone(zoneFeedback, () => {
+    currentPopup =  WA.ui.openPopup("feedbackPopup","Möchtest du ein Feedback geben?",[
+
+
+            {
+                label: 'Nein',
+                className: 'secondary',
+                callback: () => closePopUp(),
+            },
+            {
+                label: "Gerne",
+                className:"primary",
+                callback: (popup => {
+                    WA.nav.openCoWebSite(urlFeedback);
+                    isCoWebSiteOpened = true;
+                    closePopUp();
+                })
+            }]);
+    })
+
+    WA.room.onLeaveZone(zoneFeedback, () =>{
+        closePopUp();
+
+        if (isCoWebSiteOpened) {
+            WA.nav.closeCoWebSite();
+            isCoWebSiteOpened = false;
+        }
+    })
+
+    WA.room.onEnterZone(zoneDBPlanet, () => {
+    currentPopup =  WA.ui.openPopup("DBPlanetPopup","DB Planet WorkAdventure",[
+            {
+                label: "OK",
+                className:"primary",
+                callback: (popup => {
+                    WA.nav.openTab(urlDBPlanet);
+                    isCoWebSiteOpened = true;
+                    closePopUp();
+                })
+            }]);
+    })
+
+    WA.room.onLeaveZone(zoneDBPlanet, () =>{
+        closePopUp();
+
+        if (isCoWebSiteOpened) {
+            WA.nav.closeCoWebSite();
+            isCoWebSiteOpened = false;
+        }
+    })
+});
